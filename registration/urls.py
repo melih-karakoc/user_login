b55ba961bba4c9931c1b143b0cf9f744ba51abf5
Melih Karakoç <melihkarakoc@Melihs-MacBook-Pro.local>
2024-02-29 11:32:54 +0300

    added login and home page

--- registration/urls.py
+++ registration/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views 
 from rest_framework.routers import DefaultRouter
+from registration.views import CustomLoginView, RegisterView
+from registration.forms import LoginForm
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("register", views.register_attempt, name="register_attempt"),
-    path("login", views.login_attempt, name="login_attempt"),
+    # path("register", views.register_attempt, name="register_attempt"),
+    # path("login", views.login_attempt, name="login_attempt"),
+    path('login/', CustomLoginView.as_view(
+        redirect_authenticated_user=True, 
+        template_name='registration/login.html',
+        authentication_form=LoginForm), name='login'),
+    path('register/', RegisterView.as_view(), name='users-register'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView
+from django.views import View
 
 from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
 
@@ -32,3 +33,33 @@ class CustomLoginView(LoginView):
 
         # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
         return super(CustomLoginView, self).form_valid(form)
+
+class RegisterView(View):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'registration/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to access the register page while logged in
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        # else process dispatch as it otherwise normally would
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='login')
+
+        return render(request, self.template_name, {'form': form})
