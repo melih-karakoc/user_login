3822fda03c7b44a98c43130e16ddadb3e9a04d4f
Melih Karakoç <melihkarakoc@Melihs-MacBook-Pro.local>
2024-03-02 14:55:39 +0300

    strategy pattern introduced

---
+++ registration/strategies.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import login
+from registration import helpers
+
+class AuthStrategy:
+    provider_strategy_map = {
+        'google': 'GoogleStrategy'
+    }
+
+    def prepare_auth_url(self, request):
+        pass
+
+    def social_auth_callback(self, request):
+        pass
+
+
+    def get_or_create_user(self, user_info):
+        user = helpers.create_user(user_info)[0]
+        helpers.update_profile(user)
+        return user
+
+    def login_user(self, request, user):
+        login(request, user)
--- registration/tasks.py
+++ registration/tasks.py
@@ -6,7 +6,7 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 
-from .helpers import TokenGenerator
+from registration import helpers
 
 
 @shared_task(ignore_result=True, max_retries=5)
@@ -15,7 +15,7 @@ def send_verification_email(scheme, domain, user_id):
     profile = user.profile
     email = user.email
     subject = "Verify Email"
-    account_activation_token = TokenGenerator()
+    account_activation_token = helpers.TokenGenerator()
 
     message = render_to_string('registration/verify_email_message.html', {
         'scheme': scheme,
